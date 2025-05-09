1671f3891984abd2a68ebdc562a91e90bb950206
joselc2000 <20243rd042@utez.edu.mx>
2025-05-09 10:28:18 -0600

    Operaciones

---
+++ Operaciones.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+public class Numeros {
+    public static void main(String[] args){
+        //Enteros
+        String numero1 = JOptionPane.showInputDialog("Ingresa um mumero: ");
+        String numero2 = JOptionPane.showInputDialog("Ingresa un numero: ");
+        int numeroEntero1 = Integer.parseInt(numero1);
+        int numeroEntero2 = Integer.parseInt(numero2);
+        System.out.println(numeroEntero1 + numeroEntero2);
+        System.out.println(numeroEntero1 / numeroEntero2);
+        System.out.println(numeroEntero1 * numeroEntero2);
+        System.out.println(numeroEntero1 - numeroEntero2);
+       
+    }
+}
